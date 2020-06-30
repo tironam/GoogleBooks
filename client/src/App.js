5667d04600c9b5d653f28363168f7be4c1055094
Michael Tirona <tironamichael@gmail.com>
2020-06-29 23:30:47 -0700

    Added code to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -4,3 +4,22 @@ import {
   Switch,
   Route
 } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/saved'>
+            <Saved />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
