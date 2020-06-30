b6f0d30b610f15fa9ffdd31e7525caa596fd8aff
Michael Tirona <tironamichael@gmail.com>
2020-06-29 22:56:33 -0700

    Added model and routes

---
+++ models/Books.js
@@ -0,0 +1,11 @@
+const { model, Schema } = require('mongoose')
+
+const BookSchema = new Schema({
+    title: String,
+    authors: String, 
+    description: String,
+    image: String,
+    link: String
+})
+
+module.exports = model('Book', BookSchema)
