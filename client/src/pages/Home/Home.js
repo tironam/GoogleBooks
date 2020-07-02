8286d81889bf78e038778da29566ccd5bd96fa6a
Michael <tironamichael@gmail.com>
2020-07-01 20:56:10 -0700

    Added proxy to package.json

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import CardHeader from '@material-ui/core/CardHeader'
+import Avatar from '@material-ui/core/Avatar'
+import axios from 'axios'
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140, 
+    },
+})
 
 const Home = () => {
+
+    const classes = useStyles()
+    
     return (
         <h1>This is home</h1>
     )
